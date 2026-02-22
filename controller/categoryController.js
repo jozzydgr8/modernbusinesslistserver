7ba6e7dac4d9b5a01a8a19c58235c52a42262540
jozzydgr8 <jozzydgreat1@gmail.com>
2026-02-22 01:24:12 -0800

    added category and subcategory fetch

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,4 +1,5 @@
-const Category = require('../Schema/categorySchema')
+const Category = require('../Schema/categorySchema');
+const SubCategory = require('../Schema/subCategorySchema');
 
 const getCategories = async(req,res)=>{
     try{
@@ -24,5 +25,23 @@ const createCategory = async(req,res)=>{
     }
 }
 
+const getCategorywithSubCategories = async (req, res)=>{
+    try{
+        //fetch categories
+        const fetchCategories = await Category.find({}).lean();
 
-module.exports={getCategories, createCategory}
+        //fetch subcategories
+        const fetchSubCategories = await SubCategory.find({}).lean();
+
+        //attach subcategories to categories
+        const result = fetchCategories.map(cat => ({
+      ...cat,
+      subCategories: fetchSubCategories.filter(sub => sub.categoryId.toString() === cat._id.toString())
+    }));
+        res.status(200).json(result);
+    }catch(error){
+        res.status(400).json(error.message)
+    }
+}
+
+module.exports={getCategories, createCategory, getCategorywithSubCategories}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const {getCategories, createCategory} = require('../controller/categoryController');
+const {getCategories, createCategory, getCategorywithSubCategories} = require('../controller/categoryController');
 const subCategoryRoutes = require('./subCategoryRoute');
 
 router.get('/',getCategories);
 router.post('/',createCategory);
+router.get('/withSubCategories',getCategorywithSubCategories)
 
 // nest routing of subcategories as children to categories under or using unique id
 router.use('/:categoryId/subCategories', subCategoryRoutes);
